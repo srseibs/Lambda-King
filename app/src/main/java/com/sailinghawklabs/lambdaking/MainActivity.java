32c0c2c19cf026c217e14b9c1bf20c7fc74ea94b
Mike <srseibs+git@gmail.com>
2018-06-26 20:11:51 -0700

    Initial commit

---
+++ app/src/main/java/com/sailinghawklabs/lambdaking/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sailinghawklabs.lambdaking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
