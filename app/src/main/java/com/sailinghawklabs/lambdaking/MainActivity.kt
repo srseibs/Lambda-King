9ddfc10be65c87dcbf42c16eed3178aabcf7f8f4
Mike Seibel <mike@SailingHawkLabs.com>
2023-01-14 12:17:25 -0800

    Refactored deprecated startActivityForResult

--- app/src/main/java/com/sailinghawklabs/lambdaking/MainActivity.kt
+++ app/src/main/java/com/sailinghawklabs/lambdaking/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.sailinghawklabs.lambdaking
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -14,6 +14,8 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
@@ -22,16 +24,18 @@ import com.sailinghawklabs.engineeringnotation.EngineeringNotationTools
 import com.sailinghawklabs.lambdaking.databinding.ActivityMainBinding
 import com.sailinghawklabs.lambdaking.databinding.IncludeDataEntryBinding
 import com.sailinghawklabs.lambdaking.databinding.IncludeGridResultsBinding
-import com.sailinghawklabs.lambdaking.entities.*
+import com.sailinghawklabs.lambdaking.entities.Characters
+import com.sailinghawklabs.lambdaking.entities.EntryState
+import com.sailinghawklabs.lambdaking.entities.FreqUnits
+import com.sailinghawklabs.lambdaking.entities.LengthUnits
+import com.sailinghawklabs.lambdaking.entities.NumericEntryData
 import com.sailinghawklabs.lambdaking.tlines.SelectTlineActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private const val RESULT_CODE_TLINE_VF = 123
         private const val DEFAULT_NUM_DP = 2
-        private const val ER_VF_NUM_DP = 3
         private const val PREF_KEY_PRESET_ENTRY = "preset_key"
         private val TAG = MainActivity::class.java.simpleName
         private lateinit var presetEntryState: EntryState
@@ -76,32 +80,45 @@ class MainActivity : AppCompatActivity() {
         dataBinding.mainEtEr.setText("1")
         dataBinding.mainTvErLabel.text = Characters.EPSILON_SUB_R
 
-        dataBinding.freqUnitsSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-                freqSpinnerChanged(id)
-            }
+        dataBinding.freqUnitsSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    freqSpinnerChanged(id)
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                Log.d(TAG, "onNothingSelected: freq spinner")
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Log.d(TAG, "onNothingSelected: freq spinner")
+                }
             }
-        }
-        dataBinding.lengthUnitSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-                lengthSpinnerChanged(id)
-            }
+        dataBinding.lengthUnitSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    lengthSpinnerChanged(id)
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
         dataBinding.mainTlList.setOnClickListener {
-            val intent = Intent(this, SelectTlineActivity::class.java)
-            startActivityForResult(intent, RESULT_CODE_TLINE_VF)
+            startForTlineResult.launch(Intent(this, SelectTlineActivity::class.java))
         }
 
-        freqSpinnerAdapter = LinkedHashMapAdapter(this, android.R.layout.simple_spinner_item, FreqUnits)
+        freqSpinnerAdapter =
+            LinkedHashMapAdapter(this, android.R.layout.simple_spinner_item, FreqUnits)
         freqSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         dataBinding.freqUnitsSpinner.adapter = freqSpinnerAdapter
 
-        lengthSpinnerAdapter = LinkedHashMapAdapter(this, android.R.layout.simple_spinner_item, LengthUnits)
+        lengthSpinnerAdapter =
+            LinkedHashMapAdapter(this, android.R.layout.simple_spinner_item, LengthUnits)
         lengthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         dataBinding.lengthUnitSpinner.adapter = lengthSpinnerAdapter
 
@@ -134,20 +151,6 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private fun checkAllEntries(): Boolean {
-        if (!isEntryValid(dataBinding.mainEtFreq, "Frequency")) {
-            return false
-        }
-        if (!isEntryValid(dataBinding.mainEtLength, "Length")) {
-            return false
-        }
-        return if (epsilonIsMaster) {
-            isEntryValid(dataBinding.mainEtEr, Characters.EPSILON_SUB_R.toString())
-        } else {
-            isEntryValid(dataBinding.mainEtVf, "Velocity")
-        }
-    }
-
     private fun readEditTexts(): NumericEntryData {
         Log.d("MainActivity", "readEditTexts: epsilonIsMaster=${epsilonIsMaster}")
         val numericEntryData = NumericEntryData(false)
@@ -168,24 +171,28 @@ class MainActivity : AppCompatActivity() {
         // apply suffix multipliers from the Spinners
         var position = dataBinding.freqUnitsSpinner.selectedItemPosition
         val freqMultEntry = freqSpinnerAdapter.getItem(position)
-                ?: throw Exception("MainActivity: readEditTexts: missing freq spinner entry position: $position")
+            ?: throw Exception("MainActivity: readEditTexts: missing freq spinner entry position: $position")
         val freqMultiplier = freqMultEntry.key
         numericEntryData.frequency_Hz *= freqMultiplier
 
         position = dataBinding.lengthUnitSpinner.selectedItemPosition
         val lengthMultEntry = lengthSpinnerAdapter.getItem(position)
-                ?: throw Exception("MainActivity: readEditTexts: missing length spinner entry position: $position")
+            ?: throw Exception("MainActivity: readEditTexts: missing length spinner entry position: $position")
         val lengthMultiplier = lengthMultEntry.key
         numericEntryData.cableLength_m *= lengthMultiplier
-        Log.d(TAG, "calculateResults: frequency= " + numericEntryData.frequency_Hz + "Hz, length="
-                + numericEntryData.cableLength_m + "m, Vf = " + numericEntryData.velocityFactor)
+        Log.d(
+            TAG, "calculateResults: frequency= " + numericEntryData.frequency_Hz + "Hz, length="
+                    + numericEntryData.cableLength_m + "m, Vf = " + numericEntryData.velocityFactor
+        )
 
         if (!dataBinding.mainEtVf.text.isNullOrEmpty()) {
-            numericEntryData.velocityFactor = (Math.max(dataBinding.mainEtVf.text.toString().toDouble(), 0.001))
+            numericEntryData.velocityFactor =
+                (Math.max(dataBinding.mainEtVf.text.toString().toDouble(), 0.001))
         }
 
         if (!dataBinding.mainEtEr.text.isNullOrEmpty()) {
-            numericEntryData.epsilon = (Math.max(dataBinding.mainEtEr.text.toString().toDouble(), 0.001))
+            numericEntryData.epsilon =
+                (Math.max(dataBinding.mainEtEr.text.toString().toDouble(), 0.001))
             numericEntryData.epsilon = (Math.min(numericEntryData.epsilon, 100000.00))
         }
 
@@ -202,62 +209,99 @@ class MainActivity : AppCompatActivity() {
         }
         val autoRanger = AutoRanger(DEFAULT_NUM_DP)
         val calcs =
-                Calculator.execute(numericEntryData, epsilonIsMaster)
+            Calculator.execute(numericEntryData, epsilonIsMaster)
 
         // -------------------------------------------------------------------------------------
         // Adjust the units and display results
         // auto-units: *s, *m, ft|in|mi|mil|yd,
-        resultBinding.resultPhaseShiftDeg.text = autoRanger.rangePhase(calcs.phaseShift_deg).toEngineeringString()
+        resultBinding.resultPhaseShiftDeg.text =
+            autoRanger.rangePhase(calcs.phaseShift_deg).toEngineeringString()
         resultBinding.resultDelay.text = autoRanger.rangeTime(calcs.delay_s).toEngineeringString()
-        resultBinding.resultElen.text = autoRanger.rangeWavelengths(calcs.num_wavelens).toEngineeringString()
+        resultBinding.resultElen.text =
+            autoRanger.rangeWavelengths(calcs.num_wavelens).toEngineeringString()
 
         // speed --------------------------------------------------------------------
-        val encoded_speed_m_s = EngineeringNotationTools.encodeMantissa(calcs.velocity_m_s, DEFAULT_NUM_DP)
-        var result = encoded_speed_m_s.mantissaString + " " + encoded_speed_m_s.exponentString + "m/s"
+        val encoded_speed_m_s =
+            EngineeringNotationTools.encodeMantissa(calcs.velocity_m_s, DEFAULT_NUM_DP)
+        var result =
+            encoded_speed_m_s.mantissaString + " " + encoded_speed_m_s.exponentString + "m/s"
         resultBinding.resultSpeedMS.text = result
-        val encoded_speed_mi_s = EngineeringNotationTools.encodeMantissa(calcs.velocity_mi_s, DEFAULT_NUM_DP)
+        val encoded_speed_mi_s =
+            EngineeringNotationTools.encodeMantissa(calcs.velocity_mi_s, DEFAULT_NUM_DP)
         result = encoded_speed_mi_s.mantissaString + " " + encoded_speed_m_s.exponentString + "mi/s"
         resultBinding.resultSpeedMiS.text = result
-        val encoded_speed_s_m = EngineeringNotationTools.encodeMantissa(calcs.velocity_s_m, DEFAULT_NUM_DP)
+        val encoded_speed_s_m =
+            EngineeringNotationTools.encodeMantissa(calcs.velocity_s_m, DEFAULT_NUM_DP)
         result = encoded_speed_s_m.mantissaString + " " + encoded_speed_s_m.exponentString + "s/m"
         resultBinding.resultSpeedSM.text = result
-        val encoded_speed_s_in = EngineeringNotationTools.encodeMantissa(calcs.velocity_s_in, DEFAULT_NUM_DP)
-        result = encoded_speed_s_in.mantissaString + " " + encoded_speed_s_in.exponentString + "s/in"
+        val encoded_speed_s_in =
+            EngineeringNotationTools.encodeMantissa(calcs.velocity_s_in, DEFAULT_NUM_DP)
+        result =
+            encoded_speed_s_in.mantissaString + " " + encoded_speed_s_in.exponentString + "s/in"
         resultBinding.resultSpeedSIn.text = result
 
         // lamdas -------------------------------------------------------------
-        resultBinding.resultLambdaM.text = autoRanger.rangeLength(calcs.lambda_m).toEngineeringString()
-        resultBinding.resultLambdaFt.text = autoRanger.rangeLengthImperial(calcs.lambda_m).toEngineeringString()
-        resultBinding.resultLambda2M.text = autoRanger.rangeLength(calcs.lambda_m / 2).toEngineeringString()
-        resultBinding.resultLambdaFt.text = autoRanger.rangeLengthImperial(calcs.lambda_m / 2).toEngineeringString()
-        resultBinding.resultLambda4M.text = autoRanger.rangeLength(calcs.lambda_m / 4).toEngineeringString()
-        resultBinding.resultLambda4Ft.text = autoRanger.rangeLengthImperial(calcs.lambda_m / 4).toEngineeringString()
+        resultBinding.resultLambdaM.text =
+            autoRanger.rangeLength(calcs.lambda_m).toEngineeringString()
+        resultBinding.resultLambdaFt.text =
+            autoRanger.rangeLengthImperial(calcs.lambda_m).toEngineeringString()
+        resultBinding.resultLambda2M.text =
+            autoRanger.rangeLength(calcs.lambda_m / 2).toEngineeringString()
+        resultBinding.resultLambdaFt.text =
+            autoRanger.rangeLengthImperial(calcs.lambda_m / 2).toEngineeringString()
+        resultBinding.resultLambda4M.text =
+            autoRanger.rangeLength(calcs.lambda_m / 4).toEngineeringString()
+        resultBinding.resultLambda4Ft.text =
+            autoRanger.rangeLengthImperial(calcs.lambda_m / 4).toEngineeringString()
 
         // phase slopes --------------------------------------------------
         result = autoRanger.rangeLength(calcs.phase_slope_m_deg).toEngineeringString() + "/deg"
         resultBinding.resultSlopeMDeg.text = result
-        result = autoRanger.rangeLengthImperial(calcs.phase_slope_m_deg).toEngineeringString() + "/deg"
+        result =
+            autoRanger.rangeLengthImperial(calcs.phase_slope_m_deg).toEngineeringString() + "/deg"
         resultBinding.resultSlopeFtDeg.text = result
-        val encodedSlope_deg_m = EngineeringNotationTools.encodeMantissa(1 / calcs.phase_slope_m_deg, DEFAULT_NUM_DP)
-        result = encodedSlope_deg_m.mantissaString + " " + encodedSlope_deg_m.exponentString + "deg/m"
+        val encodedSlope_deg_m =
+            EngineeringNotationTools.encodeMantissa(1 / calcs.phase_slope_m_deg, DEFAULT_NUM_DP)
+        result =
+            encodedSlope_deg_m.mantissaString + " " + encodedSlope_deg_m.exponentString + "deg/m"
         resultBinding.resultSlopeDegM.text = result
-        val encodedSlope_deg_ft = EngineeringNotationTools.encodeMantissa(1 / calcs.phase_slope_ft_deg, DEFAULT_NUM_DP)
-        result = encodedSlope_deg_ft.mantissaString + " " + encodedSlope_deg_ft.exponentString + "deg/ft"
+        val encodedSlope_deg_ft =
+            EngineeringNotationTools.encodeMantissa(1 / calcs.phase_slope_ft_deg, DEFAULT_NUM_DP)
+        result =
+            encodedSlope_deg_ft.mantissaString + " " + encodedSlope_deg_ft.exponentString + "deg/ft"
         resultBinding.resultSlopeDegFt.text = result
-        val encodedSlope_deg_hz = EngineeringNotationTools.encodeMantissa(calcs.phase_slope_deg_hz, DEFAULT_NUM_DP)
-        result = encodedSlope_deg_hz.mantissaString + " " + encodedSlope_deg_hz.exponentString + "deg/Hz"
+        val encodedSlope_deg_hz =
+            EngineeringNotationTools.encodeMantissa(calcs.phase_slope_deg_hz, DEFAULT_NUM_DP)
+        result =
+            encodedSlope_deg_hz.mantissaString + " " + encodedSlope_deg_hz.exponentString + "deg/Hz"
         resultBinding.resultSlopeDegHz.text = result
-        val encodedSlope_hz_deg = EngineeringNotationTools.encodeMantissa(1 / calcs.phase_slope_deg_hz, DEFAULT_NUM_DP)
-        result = encodedSlope_hz_deg.mantissaString + " " + encodedSlope_hz_deg.exponentString + "Hz/deg"
+        val encodedSlope_hz_deg =
+            EngineeringNotationTools.encodeMantissa(1 / calcs.phase_slope_deg_hz, DEFAULT_NUM_DP)
+        result =
+            encodedSlope_hz_deg.mantissaString + " " + encodedSlope_hz_deg.exponentString + "Hz/deg"
         resultBinding.resultSlopeHzDeg.text = result
 
         // epsilon ------------------------------------------------------------------
-        dataBinding.mainEtVf.setText(String.format(Locale.US, "%.${calcNumDps(calcs.velocityFactor)}f", calcs.velocityFactor))
-        dataBinding.mainEtEr.setText(String.format(Locale.US, "%.${calcNumDps(calcs.epsilon_r)}f", calcs.epsilon_r))
+        dataBinding.mainEtVf.setText(
+            String.format(
+                Locale.US,
+                "%.${calcNumDps(calcs.velocityFactor)}f",
+                calcs.velocityFactor
+            )
+        )
+        dataBinding.mainEtEr.setText(
+            String.format(
+                Locale.US,
+                "%.${calcNumDps(calcs.epsilon_r)}f",
+                calcs.epsilon_r
+            )
+        )
 
         // VSWR ripple spacing -------------------------------------------------------
-        val encoded_ripple_hz = EngineeringNotationTools.encodeMantissa(calcs.vswr_ripple_spacing_hz, DEFAULT_NUM_DP)
-        result = encoded_ripple_hz.mantissaString + " " + encoded_ripple_hz.exponentString + "Hz spacing"
+        val encoded_ripple_hz =
+            EngineeringNotationTools.encodeMantissa(calcs.vswr_ripple_spacing_hz, DEFAULT_NUM_DP)
+        result =
+            encoded_ripple_hz.mantissaString + " " + encoded_ripple_hz.exponentString + "Hz spacing"
         resultBinding.resultVswrRipple.text = result
     }
 
@@ -322,7 +366,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideKeyboard(view: View) {
-        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view.windowToken, 0)
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+            view.windowToken,
+            0
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -381,29 +428,33 @@ class MainActivity : AppCompatActivity() {
         refreshDisplay()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.d(TAG, "onActivityResult: requestCode == $requestCode")
-        if (requestCode == RESULT_CODE_TLINE_VF) {
-            if (resultCode == Activity.RESULT_OK) {
-                val newVf = data!!.getDoubleExtra("VF", 1.0)
-                dataBinding.mainEtVf.setText(newVf.toString())
-                hideKeyboard(dataBinding.mainEtVf)
-                dataBinding.mainEtVf.clearFocus()
-                dataBinding.mainEtEr.clearFocus()
-                dataBinding.mainEtLength.clearFocus()
-                epsilonIsMaster = false
-                refreshDisplay()
-            }
+    private val startForTlineResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            val vf = intent?.getDoubleExtra("VF", 1.0) ?: 1.0
+            updateVfFromActivityReturn(vf)
         }
     }
 
+    private fun updateVfFromActivityReturn(newVf: Double) {
+        dataBinding.mainEtVf.setText(newVf.toString())
+        hideKeyboard(dataBinding.mainEtVf)
+        dataBinding.mainEtVf.clearFocus()
+        dataBinding.mainEtEr.clearFocus()
+        dataBinding.mainEtLength.clearFocus()
+        epsilonIsMaster = false
+        refreshDisplay()
+    }
+
+    @SuppressLint("VisibleForTests")
     private fun enableAdvertisements() {
         // add physical device(s) as Test Devices for development (not release)
         if (BuildConfig.DEBUG) {
             val config = RequestConfiguration.Builder()
-                    .setTestDeviceIds(listOf("28B5489C26A46FD308BFB094FC7F36D8"))
-                    .build()
+                .setTestDeviceIds(listOf("28B5489C26A46FD308BFB094FC7F36D8"))
+                .build()
             MobileAds.setRequestConfiguration(config)
 
         }
